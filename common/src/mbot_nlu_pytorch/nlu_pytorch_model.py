fbfe0fda5bb897b568fb271d2ac3e3acacccbedb
jbarroca <joao.barroca@tecnico.ulisboa.pt>
2019-08-06 17:35:11 +0100

    modified package node script, added common libraries, added the model, added the msg

---
+++ common/src/mbot_nlu_pytorch/nlu_pytorch_model.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import, division, print_function
+
+import numpy as np
+import json
+
+import nltk
+nltk.download('punkt')
+
+from mbot_nlu_pytorch.bert import Ner
+
+
+class NLUModel(object):
+
+    def __init__(self, model_dir: str):
+
+        self.model = Ner(model_dir)
+
+
+    def predict(sentences):
+
+        preds = None
+
+        for sentence in sentences:
+
+            output = model.predict(sentence)
+
+            objs = []
+            obj_object = []
+            for word_tag_pair in output:
+                word = word_tag_pair[0]
+                tag = word_tag_pair[1]["tag"]
+                prob = word_tag_pair[1]["confidence"]
+                if tag == "Bobject":
+                    obj_object.append(word)
+                elif tag == "Iobject":
+                    obj_object.append(word)
+                else:
+                    if obj_object:
+                        objs.append(' '.join(obj_object))
+                        obj_object = []
+            if obj_object:
+                objs.append(' '.join(obj_object))
+                obj_object = []
+
+            print("\n| Order: ", sentence)
+            for o in objs:
+                w = compute_multiple_words_vector(o.split(), embedding_model)
+                true_obj = find_similar_word(w, word_encoder=items_encoder, threshold=threshold)
+                if true_obj:
+                    print("--> Known Item: ", true_obj)
+                else:
+                    print("--> Unknown Item: ", o)
+                    # when I dont know an item I can ask for confirmation and then add it to the know items
+                    #new_dict = {o: compute_multiple_words_vector(o.split(), embedding_model)}
+                    #items_encoder.update(new_dict)
+
+        return preds
--- ros/src/mbot_nlu_pytorch_ros/mbot_nlu_pytorch_node.py
+++ ros/src/mbot_nlu_pytorch_ros/mbot_nlu_pytorch_node.py
@@ -1,6 +1,39 @@
 #!/usr/bin/env python
 
-from mbot_nlu_pytorch.my_ros_independent_class import my_generic_sum_function
+import os
+
+import rospy
+import rospkg
+
+from mbot_nlu_pytorch.nlu_pytorch_model import NLUModel
+
 
 def main():
- print 'success'
+
+    rospack = rospkg.RosPack()
+    pkg_dir = rospack.get_path("mbot_nlu_pytorch")
+    model_dir = os.path.join(pkg_dir, "common/src/model")
+
+    nlu_model = NLUModel(model_dir=model_dir)
+
+    sentences = [
+        "I want a coffee",
+        "bring me a beer",
+        "I would like a coke",
+        "a cup of tea",
+        "I want a breakfast wrap and a coffee please",
+        "can you bring me a chicken salad sandwich please",
+        "I want a toast with ham and cheese",
+        "I want the innocent mango",
+        "I want a smoothie",
+        "I want an italian biscotti",
+        "I want a large cup of coffee",
+        "I want a medium cup of tea",
+        "I want a small cup",
+        "I want some crisps",
+        "I want a veggie pot",
+        "bring me a water"
+    ]
+
+    nlu_model.predict(sentences)
+
