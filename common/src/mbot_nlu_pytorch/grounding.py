c43c88fc630d092a2471424a2c683d202d84cd3f
João Barroca <joaobarroca93@gmail.com>
2019-08-16 16:56:38 +0100

    added grounding to nlu model

--- common/src/mbot_nlu_pytorch/grounding.py
+++ common/src/mbot_nlu_pytorch/grounding.py
@@ -10,18 +10,35 @@ nltk.download('punkt')
 
 class GroundingModel(object):
 
-    def __init__(self, known_words, embedding_model_dir="glove-wiki-gigaword-50",
-                 threshold=0.7):
+    def __init__(self, known_words, model_dir="glove-wiki-gigaword-50",
+                 threshold=0.8, remove_stop_words=True, do_lower_case=True):
 
+        self.do_lower_case = do_lower_case
+        self.remove_stop_words = remove_stop_words
         self.known_words = known_words
-        self.embedding_model = api.load(embedding_model_dir)
-        self.stop_words = set(stopwords.words('english'))
+        self.embedding_model = api.load(model_dir)
+
+        # Stop words for removing from NER recognition (CHANGE TO INCORPORATE OUR OWN STOPWORDS IN THE ONTOLOGY!!)
+        if self.remove_stop_words:
+            self.stop_words = set(stopwords.words('english'))
+            # Remove any known words from the stopwords
+            [self.stop_words.remove(word) for word in self.known_words if word in self.stop_words]
+            self.stop_words.add("please")
+
         self.word_encoder = self._encode_known_words()
         self.threshold = threshold
 
     def _compute_multiple_words_vector(self, words):
+
+        if self.do_lower_case:
+            words = [word.lower() for word in words]
+
         words_in_vocab = [word for word in words if word in self.embedding_model.vocab.keys()]
-        return [np.sum(self.embedding_model[word] for word in words_in_vocab)][0]
+
+        if words_in_vocab:
+            return [np.sum(self.embedding_model[word] for word in words_in_vocab)][0]
+        else:
+            return np.zeros(self.embedding_model.vector_size)
 
     def _find_similar_word(self, w):
         possible_object = None
@@ -29,24 +46,34 @@ class GroundingModel(object):
         for word in self.word_encoder.keys():
             cosine_similarity = np.dot(w, self.word_encoder[word]) / (np.linalg.norm(w) * np.linalg.norm(
                 self.word_encoder[word]))
-
-            if cosine_similarity >= self.threshold and cosine_similarity >= max_cosine_sim:
-                possible_object = word
-                max_cosine_sim = cosine_similarity
-        return (possible_object, max_cosine_sim)
+            try:
+                if cosine_similarity >= self.threshold and cosine_similarity >= max_cosine_sim:
+                    possible_object = word
+                    max_cosine_sim = cosine_similarity
+            except ValueError:
+                raise ValueError("ERROR IN FIND SIMILAR WORD (GROUNDING) !!")
+        return possible_object
 
     def _encode_known_words(self):
         return {word: self._compute_multiple_words_vector(
             word.split()) for word in self.known_words}
 
-    def predict(self, item):
-        words = [word for word in item.split() if word not in self.stop_words]
+    def predict(self, item_words):
+        if self.remove_stop_words:
+            words = [word for word in item_words if word not in self.stop_words]
+        else:
+            words = item_words
         w = self._compute_multiple_words_vector(words)
         return self._find_similar_word(w)
 
 
 if __name__ == '__main__':
-    ground_model = GroundingModel(known_words=["beer", "coke", "fridge", "laptop", "book"], threshold=0.7)
+
+    known_items = ["beer", "coke", "fridge", "laptop", "book"]
+
+    ground_model = GroundingModel(known_words=known_items, threshold=0.7,
+                                  model_dir="glove-wiki-gigaword-50",
+                                  remove_stop_words=True)
 
     while True:
         try:
--- common/src/mbot_nlu_pytorch/nlu_pytorch_model.py
+++ common/src/mbot_nlu_pytorch/nlu_pytorch_model.py
@@ -3,15 +3,21 @@
 from __future__ import absolute_import, division, print_function
 
 import json
+import ipdb
 
-from mbot_nlu_pytorch.modeling import BertClassificationInference, BertNerInference
+#from mbot_nlu_pytoch.modeling import BertClassificationInference, BertNerInference
+#from mbot_nlu_pytoch.grounding import GroundingModel
+
+from modeling import BertClassificationInference, BertNerInference
+from grounding import GroundingModel
 
 
 def reset_d_act():
     return {
         "d-type": None,
         "intent": None,
-        "args": {"object": [], "person": [], "destination": [], "source": [], "what_to_tell": []}
+        "known_args": {"object": [], "person": [], "destination": [], "source": [], "what_to_tell": []},
+        "unknown_args": {"object": [], "person": [], "destination": [], "source": [], "what_to_tell": []}
     }
 
 
@@ -44,16 +50,30 @@ def process_ner_pred(ner_preds):
 
 class NLUModel(object):
 
-    def __init__(self, dtype_model_dir, intent_model_dir, slot_filing_model_dir):
+    def __init__(self, dtype_model_dir, intent_model_dir, slot_filing_model_dir,
+                 ontology_dir, grounding_model_dir=None):
+
+        with open(ontology_dir, "r") as fp:
+            ontology = json.load(fp)
 
         self.dtype_model = BertClassificationInference(model_dir=dtype_model_dir)
         self.intent_model = BertClassificationInference(model_dir=intent_model_dir)
         self.slot_filing_model = BertNerInference(model_dir=slot_filing_model_dir)
 
+        if grounding_model_dir:
+            self.ground_model = GroundingModel(known_words=ontology["known_words"], model_dir=grounding_model_dir)
+
     def predict(self, sentences):
 
+        # If it is only one sentence, wrap it in a list for prediction loop
+        if isinstance(sentences, str):
+            sentences = [sentences]
+
+        # Prediction Loop
         d_acts = []
         for sentence in sentences:
+            d_act = reset_d_act()
+
             dtype_pred = self.dtype_model.predict(sentence)
             intent_pred = self.intent_model.predict(sentence)
             slot_filing_pred = self.slot_filing_model.predict(sentence)
@@ -65,17 +85,56 @@ class NLUModel(object):
 
                 d_act["intent"] = {intent_pred["label"]: intent_pred["confidence"]}
 
-                for pred in preds:
-                    slot = list(pred.keys())[0]
-                    value = list(pred.values())[0]
-                    conf = list(pred.values())[1]
-                    d_act["args"][slot].append({' '.join(value): conf})
+                try:
+                    for pred in preds:
+                        slot = list(pred.keys())[0]
+                        value = list(pred.values())[0]
+                        conf = list(pred.values())[1]
+                        true_value = None
+
+                        if self.ground_model:
+                            true_value = self.ground_model.predict(value)
+
+                        if true_value:
+                            d_act["known_args"][slot].append({true_value: conf})
+
+                        else:
+                            d_act["unknown_args"][slot].append({' '.join(value): conf})
+
+                except ValueError:
+                    ipdb.set_trace()
+                    raise ValueError("ERROR ON PROCESSING NER PREDICTIONS")
+
+                except IndexError:
+                    ipdb.set_trace()
+                    raise IndexError("ERROR ON INDEX")
 
             # change from <print> to <logging.debug>
-            print("SENTENCE: ", sentence)
             print(json.dumps(d_act, indent=4))
 
             d_acts.append(d_act)
-            d_act = reset_d_act()
 
         return d_acts
+
+
+if __name__ == '__main__':
+
+    nlu_model = NLUModel(
+        dtype_model_dir="../model/dtype",
+        intent_model_dir="../model/intent",
+        slot_filing_model_dir="../model/slotfiling",
+        grounding_model_dir="glove-wiki-gigaword-50",
+        ontology_dir="../model/ontology.json"
+    )
+
+    while True:
+        try:
+            print("=" * 20)
+            sentence = input("SENTENCE: ")
+
+            d_act = nlu_model.predict(sentence)
+
+            #print(json.dumps(d_act, indent=4))
+
+        except KeyboardInterrupt:
+            continue
